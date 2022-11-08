e4c11d80ff392782d8723a7af01e4dcfad19c5c2
PrairieFire2b <110272607+PrairieFire2b@users.noreply.github.com>
2022-11-08 10:14:11 +0800

    Create beforePlayerSleep.ts

---
+++ tscripts/server-plus/beforePlayerSleep.ts
@@ -0,0 +1,27 @@
+import { world, Player } form "@minecraft/server"
+import { EventSignal } from './EventSignal'
+export class BeforePlayerSleepEventSignal {}
+export class BeforePlayerSleepEvent {
+    player?: Player;
+    cancel = false;
+}
+export default const signal = new BeforePlayerSleepEventSignal();
+world.beforeItemUseOn.subscribe(arg => {
+    let block = arg.source.dimension.getBlock(arg.blockLocation);
+    if(!arg.source.isSneaking
+    && block?.typeId === MinecraftBlockTypes.bed.id
+    && block?.dimension.id === "minecraft:overworld"
+    && world.getTime() >= 13000 && world.getTime() <= 23456) {
+        event = new BeforePlayerSleepEvent;
+        event.block = block;
+        event.player = Array.from(world.getPlayers({name: arg.source.nameTag}))[0];
+        signal.trigger(event);
+        arg.cancel = event.cancel;
+    }
+});
+
+export {
+    BeforePlayerSleepEventSignal,
+    BeforePlayerSleepEvent,
+    signal as beforePlayerSleep
+}
